91a553955ffe652006c0734b71a1617ab575f5a6
shota <30305829kasukabe@gmail.com>
2024-09-09 20:07:16 +0900

    ディレクトリ構成変更

--- coffeeNoteApi/urls.py
+++ coffeeNoteApi/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recipes/', include('recipe.urls'))
+    path('recipes/', include('coffeeNoteApi.recipe.urls'))
 ]
